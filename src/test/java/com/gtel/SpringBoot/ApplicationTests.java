d98e302c51b67d0ac29d56a6795e0e533636e4cb
trungsliver <buiductrung.cva@gmail.com>
2025-08-28 10:02:32 +0700

    first commit

---
+++ src/test/java/com/gtel/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtel.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
